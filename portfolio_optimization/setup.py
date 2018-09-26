048d7414d8b198dc073485f2625d45e8ba3d2337
Johnew Zhang <jzhang19@gsb.columbia.edu>
2018-09-25 21:19:04 -0400

    Add files via upload

---
+++ portfolio_optimization/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='portfolio_optimization',
+	  version='0.0.1',
+	  url='https://github.com/WizardKingZ/portfolio_optimization',
+	  author='WizardKingZ',
+	  author_email='johnew.zhang@columbia.edu',
+	  license='MIT',
+	  packages=['portfolio_optimization'],
+	  zip_safe=False)
